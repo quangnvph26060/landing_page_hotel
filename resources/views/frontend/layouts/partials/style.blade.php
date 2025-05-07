52e0a43ce85d70cca920d845525b9810cb2c9307
thangngo.dev <thangngo130802@gmail.com>
2025-05-07 15:33:39 +0700

    a

--- resources/views/frontend/layouts/partials/style.blade.php
+++ resources/views/frontend/layouts/partials/style.blade.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}?v={{ filemtime(public_path('frontend/css/style.css')) }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.css') }}?v={{ filemtime(public_path('frontend/css/bootstrap.css')) }}">
