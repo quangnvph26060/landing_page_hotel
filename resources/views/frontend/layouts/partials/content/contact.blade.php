008fce244a196525fc23c3d5eba0d48c061d7d69
thangngo.dev <thangngo130802@gmail.com>
2025-05-14 11:17:26 +0700

    a

--- resources/views/frontend/layouts/partials/content/contact.blade.php
+++ resources/views/frontend/layouts/partials/content/contact.blade.php
@@ -115,15 +115,15 @@
             <h5 class="fw-bold text-danger mb-4">ĐĂNG KÝ NGAY<br>ĐỂ HƯỞNG ƯU ĐÃI HẤP DẪN</h5>
             <div class="info-box">
                 <i class="fas fa-hotel"></i>
-                <span>Thông tin ưu đãi 1</span>
+                <span>{{ $config->company }}</span>
             </div>
             <div class="info-box">
                 <i class="fas fa-cogs"></i>
-                <span>Thông tin ưu đãi 2</span>
+                <span>{{ $config->hotline }}</span>
             </div>
             <div class="info-box">
                 <i class="fas fa-headset"></i>
-                <span>Thông tin ưu đãi 3</span>
+                <span>{{ $config->salesPhone }}</span>
             </div>
         </div>
     </div>
