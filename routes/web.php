3868dc824ea61fd4873c9afeadcf433f99a1410f
thangngo.dev <thangngo130802@gmail.com>
2025-06-04 10:53:41 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AdvisoriesController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\User\HomeController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,7 +32,3 @@ Route::get('dang-ky-tai-khoan', [AuthController::class, 'registerUser'])->name('
 Route::post('dang-ky-tai-khoan', [AuthController::class, 'registerUserSubmit'])->name('submit.register');
 Route::post('tu-van', [AdvisoriesController::class, 'registerAdvisories'])->name('register.advisories');
 
-Route::view('upload', 'upload');
-Route::post('upload', function (Request $request) {
-    return response()->json($request->toArray());
-})->name('upload-image');
