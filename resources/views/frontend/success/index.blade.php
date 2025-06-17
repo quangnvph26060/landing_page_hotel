62614c9d9a5f19032059d51925241bd7db29a131
thangngo.dev <thangngo130802@gmail.com>
2025-06-17 10:07:19 +0700

    a

--- resources/views/frontend/success/index.blade.php
+++ resources/views/frontend/success/index.blade.php
@@ -7,7 +7,7 @@
     <title>Khách sạn của bạn đã sẵn sàng</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-
+    <link rel="icon" type="image/x-icon" href="{{ asset('storage/' . $config_all->icon) }}" sizes="64x64" fetchpriority="high">
     <style>
         body {
             margin: 0;
